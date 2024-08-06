eee8ebb71ea410ea48bfedb2e1aa94345ff55aaf
Yamanadm <adsamu44@gmail.com>
2024-08-06 04:07:34 +0300

    axios, useEffect

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../css/Advice.css";
+import axios from "axios";
+
+const BASE_URL = "https://api.adviceslip.com/advice";
 
 const Advice = () => {
-  return <div>Advice</div>;
+  const [info, setInfo] = useState("");
+
+  useEffect(() => {
+    getAdvice();
+  });
+
+  const getAdvice = async () => {
+    try {
+      const response = await axios.get(BASE_URL);
+      const { advice } = response.data.slip;
+
+      setInfo(advice);
+      // console.log(advice);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(info);
+  return (
+    <div className="advice-container">
+      <p>{info}</p>
+      <button onClick={getAdvice}>Change</button>
+    </div>
+  );
 };
 
 export default Advice;
--- src/components/Money.jsx
+++ src/components/Money.jsx
@@ -1,32 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Money.css";
 import { FaArrowCircleRight } from "react-icons/fa";
+import axios from "axios";
+
+const BASE_URL = "https://api.freecurrencyapi.com/v1/latest";
+const API_KEY = "fca_live_0WPxaSF04amCNBqJvWjN5wxdCzz6Au6RuOtAum30";
 
 const Money = () => {
+  const [amount, setAmount] = useState(0);
+  const [res, setRes] = useState(0);
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("TRY");
+
+  const getData = async () => {
+    const response = await axios.get(
+      `${BASE_URL}?apikey=${API_KEY}&base_currency=${from}`
+    );
+    setRes((amount * response.data.data[to]).toFixed(2));
+    // console.log(response.data.data[to]);
+  };
+
   return (
     <div className="money-container">
       <div className="money-text">
         <h2>Currency Calculation</h2>
       </div>
       <div style={{ marginTop: "1rem" }}>
-        <input type="number" className="amount" />
-        <select className="fromMoney" name="money" id="Money">
-          <option value="dolar">USD</option>
-          <option value="tl">TRY</option>
-
-          <option value="euro">EURO</option>
+        <input
+          type="number"
+          className="amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <select className="fromMoney" onChange={(e) => setFrom(e.target.value)}>
+          <option value="USD">USD</option>
+          <option value="TRY">TRY</option>
+          <option value="EUR">EUR</option>
         </select>
         <FaArrowCircleRight size={15} />
-        <select className="toMoney" name="money" id="Money">
-          <option value="tl">TRY</option>
-          <option value="dolar">USD</option>
-
-          <option value="euro">EURO</option>
+        <select className="toMoney" onChange={(e) => setTo(e.target.value)}>
+          <option value="TRY">TRY</option>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
         </select>
-        <input type="number" className="result-amount" />
+        <input type="number" className="result-amount" value={res} />
       </div>
       <div>
-        <button className="money-button">Calc</button>
+        <button onClick={getData} className="money-button">
+          Calc
+        </button>
       </div>
     </div>
   );
